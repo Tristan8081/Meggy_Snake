846c5895dfce127d3827b2fafa19a6ccf47d7021
Tristan Scharfenstein-Montgomery <tristanrockshawaii@gmail.com>
2016-10-28 13:46:01 -1000

    Add Game Over
    Added a white x at the end if the snake hits itself

--- MeggyJr_Blink_Project/MeggyJr_Blink_Project.ino
+++ MeggyJr_Blink_Project/MeggyJr_Blink_Project.ino
@@ -42,6 +42,7 @@ int xapple = random(8);
 int yapple = random(8);
 int binary = 0;
 int speed = 200;
+boolean gameOver = false;
 
 struct Point
 {
@@ -65,24 +66,37 @@ void setup()                    // run once, when the sketch starts
 
 void loop()                     // run over and over again
 {
- CheckButtonsDown();
- if (Button_Up)
- {
-  direction = 0;
- }
- if (Button_Right)
- {
-  direction = 90;
- }
- if (Button_Down)
- {
-  direction = 180;
- }
+  if (gameOver == false)
+  {
+    CheckButtonsDown();
+    if (Button_Up)
+    {
+        direction = 0;
+    }
+    if (Button_Right)
+    {
+        direction = 90;
+    }
+    if (Button_Down)
+    {
+       direction = 180;
+     }
  if (Button_Left)
  {
   direction = 270;
  }
+
  updateSnake();
+ 
+ 
+ // Checks to eee if snake collides with itself
+ if ( collide() )
+ {
+  Tone_Start(500,100);
+  speed = 0;
+  gameOver = true;
+ }
+ 
  DrawPx(xapple, yapple, Red);
  //Have we eaten apple?
  if (ReadPx(snakeArray[0].x,snakeArray[0].y) == Red)
@@ -91,12 +105,12 @@ void loop()                     // run over and over again
    yapple = random(8);
    Tone_Start(18182,50);
    binary = binary * 2 + 1;
+   marker ++;                      // Add one body parts to the snake.
    if (binary > 255)
    {
     binary = 0;                       // Restart to zero LED lights
     Tone_Start(10000,100);           // Beep after eight apples eaten
     speed = speed - 10;               // Change Speed
-    marker ++;                      // Add one body part to the snake.
    }
  }
  drawSnake();
@@ -104,6 +118,9 @@ void loop()                     // run over and over again
  DisplaySlate(); 
  delay(200);
  ClearSlate();
+  }
+
+  else gameOverScreen();
 }
 
 //checks the direction and updates the x or y value.
@@ -158,7 +175,42 @@ void drawSnake()
   for (int i = 0; i < marker; i++)
   {
     DrawPx(snakeArray[i].x, snakeArray[i].y,Blue);
+    DrawPx(snakeArray[0].x, snakeArray[0].y,Yellow);
   }
 }
 
+boolean collide()
+{
+  // Compare the position of the head with every other body segment
+  // if match, return true
+  for (int i = 1; i < marker; i++)
+  {
+    if (snakeArray[0].x == snakeArray[i].x && snakeArray[0].y == snakeArray[i].y)
+    {
+      return true;
+    }
+  }
+  // If no collision, return false.
+  return false;
+}
 
+void gameOverScreen()
+{
+  DrawPx(0,0,White);
+  DrawPx(1,1,White);
+  DrawPx(2,2,White);
+  DrawPx(3,3,White);
+  DrawPx(4,4,White);
+  DrawPx(5,5,White);
+  DrawPx(6,6,White);
+  DrawPx(7,7,White);
+  DrawPx(0,7,White);
+  DrawPx(1,6,White);
+  DrawPx(2,5,White);
+  DrawPx(3,4,White);
+  DrawPx(4,3,White);
+  DrawPx(5,2,White);
+  DrawPx(6,1,White);
+  DrawPx(7,0,White);
+  DisplaySlate();
+}
